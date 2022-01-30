77c48b81829d295bb7679dd823ba6d8d0857cc96
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-30 20:56:46 -0300

    Version with list size and number range

--- main.py
+++ main.py
@@ -14,21 +14,35 @@
 # na ordem errada. A passagem pela lista é repetida
 # até que a lista seja ordenada.
 
-list_of_numbers = [3, 9, 2, 1, 4, 0, 6, 8, 5, 7]
-x = 0
+from random import randint
+
+list_of_numbers = []
+counter = 0
 list_changed = False
 
-while x < (len(list_of_numbers) - 1):
+print("")
+list_size = int(input("How many numbers do you want on your list?\n"))
+print("Do you want the values on your list to vary between which numbers?")
+first_number = int(input("Type first number:\n"))
+second_number = int(input("Type second number:\n"))
 
-    if list_of_numbers[x] > list_of_numbers[x + 1]:
+for i in range(list_size):
+    list_of_numbers.append(randint(first_number, second_number))
+
+print("")
+print(f"This is the starting list: {list_of_numbers}")
+
+while counter < (len(list_of_numbers) - 1):
+
+    if list_of_numbers[counter] > list_of_numbers[counter + 1]:
         list_changed = True
-        temp = list_of_numbers[x]
-        list_of_numbers[x] = list_of_numbers[x + 1]
-        list_of_numbers[x + 1] = temp
-    x += 1
+        temp = list_of_numbers[counter]
+        list_of_numbers[counter] = list_of_numbers[counter + 1]
+        list_of_numbers[counter + 1] = temp
+    counter += 1
 
-    if x == (len(list_of_numbers) - 1) and list_changed == True:
-        x = 0
+    if counter == (len(list_of_numbers) - 1) and list_changed == True:
+        counter = 0
         list_changed = False
 
-print(list_of_numbers)
+print(f"This is the list after sorting: {list_of_numbers}")
