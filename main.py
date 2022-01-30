7e9ff2487527b1e99a535cf08524c22e6258308e
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-30 20:13:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# I saw some people talking about bubble sort
+# and tried to make it. Accordig to Wikipedia,
+# bubble sort, is a simple sorting algorithm
+# that repeatedly steps through a list,
+# compares adjacent elements and swaps them
+# if they are in the wrong order. The pass through
+# the list is repeated until the list is sorted.
+
+# Eu vi algumas pessoas falando sobre bubble sort
+# e tentei fazer um. De acordo com a Wikipédia,
+# bubble sort, é um algoritmo de ordenação simples
+# que percorre repetidamente uma lista, compara
+# elementos adjacentes e os troca se estiverem
+# na ordem errada. A passagem pela lista é repetida
+# até que a lista seja ordenada.
